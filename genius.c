919b6d8fbfa94b0ca6a4b7393c668bd385e3463c
Cristina Souza <cristinaribs@gmail.com>
2018-06-07 16:32:55 -0300

    Update genius.c

--- genius.c
+++ genius.c
@@ -1,13 +1,22 @@
-/*Jogo em si*/
-
-#include <stdio.h>
 #include <SDL.h>
-#include <g_func.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+//Constantes de dimensao da tela
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main (int argc, char* args[])
+{
+    //Inicializa SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    //Codigo
 
 
-int main (int argc, char* args[]){
-  
-  
-  
-  return 0;
+
+    //Termina SDL
+    SDL_Quit();
+
+    return 0;
 }
