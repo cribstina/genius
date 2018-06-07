d0a63a56d2520bb75a5e16946115d7975d8b336b
Cristina Souza <cristinaribs@gmail.com>
2018-06-07 16:31:43 -0300

    Update g_func.h

--- g_func.h
+++ g_func.h
@@ -1,30 +1,34 @@
 /* Declara funcoes necessarias ao funcionamento do GENIUS */
 
-//inicia o jogo apos o usuario apertar o botao do meio
+//Descricao: inicia o SDL e carrega tela
+bool inicia();
+
+//Descricao: carrega midia
+bool loadMedia();
+
+//Descricao: inicia o jogo apos o usuario apertar o botao do meio
 //imagem meio_down.bmp quando aperta o botão
 void comeca_jogo();
 
-//tudo pisca
-//imagem tudo_aceso.bmp quando tudo acende
-void tudo_acende();
 
-//cores piscam individualmente
-//imagens com o nome assim: cor_aceso.bmp com o nome da cor correspondente no lugar de "cor"
+//Descricao: cores piscam individualmente de acordo com o numero gerado pelo programa
+//imagens com o nome assim: cor_acesa.bmp com o nome da cor correspondente no lugar de "cor"
 void azul_pisca();
 void vermelho_pisca();
 void amarelo_pisca();
 void verde_pisca();
 
-//usuario clica nas cores, resposta dos botoes
+//Descricao: resposta dos botoes apos o clique do usuario
 //imagens com o nome assim: cor_down.bmp com o nome da cor correspondente no lugar de "cor"
 void aperta_azul();
 void aperta_vermelho();
 void aperta_amarelo();
 void aperta_verde();
 
-//usuario acerta todas as sequencias previstas
-void parabens();
 
-//usuario erra jogada, game over
+//Descricao: usuario erra jogada, game over
 //imagem acabou.bmp
 void game_over();
+
+//Libera midia e termina SDL
+void close();
